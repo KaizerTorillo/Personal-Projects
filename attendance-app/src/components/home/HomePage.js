db9da89b366c94bf87d5381226761fa4266636ec
Kaizer <kaizertorillo@gmail.com>
2024-04-24 10:41:31 +1200

    before presentation

--- attendance-app/src/components/home/HomePage.js
+++ attendance-app/src/components/home/HomePage.js
@@ -4,7 +4,7 @@ import "./homepage.css"
 function HomePage() {
     return (
         <div className="image">
-            <img src={banner} alt="banner" height="600px"/>
+            <img src={banner} alt="banner"/>
         </div>
     );
 }
